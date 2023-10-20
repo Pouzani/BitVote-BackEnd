4690727ca73e79b2a3a75ac98ea988cf9d50c6ae
Mohamed Riyad Bouzani <mriyadbouzani@gmail.com>
2023-10-20 01:24:16 +0100

    Fixed cors

--- src/main/java/com/example/demo/BitVoteApplication.java
+++ src/main/java/com/example/demo/BitVoteApplication.java
@@ -16,7 +16,7 @@ public class 	BitVoteApplication {
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200","http://ec2-13-39-82-122.eu-west-3.compute.amazonaws.com:8082/"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
 				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
